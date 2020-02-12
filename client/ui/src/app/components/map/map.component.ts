4dbb836c5f9c2f5ba7f94267840fa248e6c526ac
Ruben <rortiz18@islander.tamucc.edu>
2020-02-11 18:26:18 -0600

    added map that starts at Corpus Christi!

--- client/ui/src/app/components/map/map.component.ts
+++ client/ui/src/app/components/map/map.component.ts
@@ -1,12 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, Input, Output, EventEmitter, ElementRef } from "@angular/core";
 import { loadModules } from 'esri-loader';
 
-// app.js
-import { setDefaultOptions } from 'esri-loader';
- 
-// configure esri-loader to use version 3.31 from the ArcGIS CDN
-// NOTE: make sure this is called once before any calls to loadModules()
-setDefaultOptions({ version: '3.31' })
 
 @Component({
   selector: 'app-map',
@@ -14,23 +8,102 @@ setDefaultOptions({ version: '3.31' })
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  @Output() mapLoadedEvent = new EventEmitter<boolean>();
 
-  constructor() { }
+  // The <div> where we will place the map
+  @ViewChild("mapViewNode", { static: true }) private mapViewEl: ElementRef;
+
+  /**
+   * _zoom sets map zoom
+   * _center sets map center
+   * _basemap sets type of map
+   * _loaded provides map loaded status
+   */
+  private _zoom = 10;
+  private _center: Array<number> = [0.1278, 51.5074];
+  private _basemap = "streets";
+  private _loaded = false;
+  private _view: any = null;
+
+  get mapLoaded(): boolean {
+    return this._loaded;
+  }
+
+  @Input()
+  set zoom(zoom: number) {
+    this._zoom = zoom;
+  }
+
+  get zoom(): number {
+    return this._zoom;
+  }
+
+  @Input()
+  set center(center: Array<number>) {
+    this._center = center;
+  }
+
+  get center(): Array<number> {
+    return this._center;
+  }
+
+  @Input()
+  set basemap(basemap: string) {
+    this._basemap = basemap;
+  }
+
+  get basemap(): string {
+    return this._basemap;
+  }
+
+  constructor() {}
+
+  async initializeMap() {
+    try {
+      // Load the modules for the ArcGIS API for JavaScript
+      const [EsriMap, EsriMapView] = await loadModules([
+        "esri/Map",
+        "esri/views/MapView"
+      ]);
+
+      // Configure the Map
+      const mapProperties: any = {
+        basemap: this._basemap
+      };
+
+      const map: any = new EsriMap(mapProperties);
+
+      // Initialize the MapView
+      const mapViewProperties: any = {
+        container: this.mapViewEl.nativeElement,
+        center: this._center,
+        zoom: this._zoom,
+        map: map
+      };
+
+      this._view = new EsriMapView(mapViewProperties);
+      await this._view.when();
+      return this._view;
+    } catch (error) {
+      console.log("EsriLoader: ", error);
+    }
+  }
 
   ngOnInit() {
-    loadModules(['esri/map'])
-    .then(([Map]) => {
-      // create map with the given options at a DOM node w/ id 'mapNode'
-      let map = new Map('mapNode', {
-        center: [-118, 34.5],
-        zoom: 8,
-        basemap: 'hybrid'
-      });
-    })
-    .catch(err => {
-      // handle any script or module loading errors
-      console.error(err);
+    // Initialize MapView and return an instance of MapView
+    this.initializeMap().then(mapView => {
+      // The map has been initialized
+      console.log("mapView ready: ", this._view.ready);
+      this._loaded = this._view.ready;
+      this.mapLoadedEvent.emit(true);
     });
+  }
+
+  ngOnDestroy() {
+    if (this._view) {
+      // destroy the map view
+      this._view.container = null;
+    }
   }
 
 }
--- client/ui/src/app/pages/display/display.component.ts
+++ client/ui/src/app/pages/display/display.component.ts
@@ -9,6 +9,15 @@ export class DisplayComponent implements OnInit {
 
   constructor() { }
 
+  mapCenter = [-97.396378, 27.800583];
+  basemapType = 'satellite';
+  mapZoomLevel = 12;
+
+  // See app.component.html
+  mapLoadedEvent(status: boolean) {
+    console.log('The map loaded: ' + status);
+  }
+
   ngOnInit() {
   }
 
