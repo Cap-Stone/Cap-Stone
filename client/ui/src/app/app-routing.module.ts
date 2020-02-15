ac31c715e353eb7480121d2626ca3146bbe80291
Ruben <rortiz18@islander.tamucc.edu>
2020-02-14 18:13:35 -0600

    adding page routing

--- client/ui/src/app/app-routing.module.ts
+++ client/ui/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DisplayComponent } from '../app/pages/display/display.component';
+import { DisplayComponent } from './pages/display/display.component';
+import { HomeComponent } from './pages/home/home.component';
+import { OnBoardingComponent } from './pages/on-boarding/on-boarding.component';
+import { StatisticsComponent } from './pages/statistics/statistics.component';
+import { AboutComponent } from './pages/about/about.component';
 
 
 const routes: Routes = [
   {path: 'display', component: DisplayComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'on-boarding', component: OnBoardingComponent},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
