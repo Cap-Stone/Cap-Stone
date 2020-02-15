fabeb255813f85de9d9615e928e41763e384f8f7
Ruben <rortiz18@islander.tamucc.edu>
2020-02-14 18:08:30 -0600

    added new pages for production

---
+++ client/ui/src/app/pages/on-boarding/on-boarding.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OnBoardingComponent } from './on-boarding.component';
+
+
+
+@NgModule({
+  declarations: [OnBoardingComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class OnBoardingModule { }
