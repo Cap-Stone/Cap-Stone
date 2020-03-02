ba84e8bb5fb7b77a1a6df83650e7d5eb0aa9d042
Ruben <rortiz18@islander.tamucc.edu>
2020-03-02 15:43:32 -0600

    added angular material to all of our pages and made the base map type interchangeable to give the user the option of whichever map type they want to use!

--- client/ui/src/app/pages/on-boarding/on-boarding.module.ts
+++ client/ui/src/app/pages/on-boarding/on-boarding.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OnBoardingComponent } from './on-boarding.component';
 
+// Angular Material
+import { AngularMaterialModule } from 'src/app/angular-material/angular-material.module';
 
 
 @NgModule({
   declarations: [OnBoardingComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularMaterialModule
   ]
 })
 export class OnBoardingModule { }
--- client/ui/src/app/pages/statistics/statistics.module.ts
+++ client/ui/src/app/pages/statistics/statistics.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatisticsComponent } from './statistics.component';
 
-
+// Angular Material 
+import { AngularMaterialModule } from 'src/app/angular-material/angular-material.module';
 
 @NgModule({
   declarations: [StatisticsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AngularMaterialModule
   ]
 })
 export class StatisticsModule { }
