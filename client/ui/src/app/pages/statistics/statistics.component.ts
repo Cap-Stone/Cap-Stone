e7d2bcf2e4763241cceefea85f91a9371650c530
Ruben <rortiz18@islander.tamucc.edu>
2020-03-03 16:52:58 -0600

    added canvasjs charts library to statistics page to be used

--- client/ui/src/app/pages/statistics/statistics.component.ts
+++ client/ui/src/app/pages/statistics/statistics.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+ 
+// Importing CanvasJS 
+import * as CanvasJS from './canvasjs.min';
+
+// More information and documentation can be found at 
+// https://canvasjs.com/angular-charts/
 
 @Component({
   selector: 'app-statistics',
@@ -10,6 +16,6 @@ export class StatisticsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log('CanvasJS: ', CanvasJS);
   }
-
 }
