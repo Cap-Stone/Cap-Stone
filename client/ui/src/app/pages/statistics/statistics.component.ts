d137d6e00749785bd57da0003995b5ea84f1a5f6
mhanson15 <matthanson14e@gmail.com>
2020-03-24 17:26:31 -0500

    Stats page with dummy data

--- client/ui/src/app/pages/statistics/statistics.component.ts
+++ client/ui/src/app/pages/statistics/statistics.component.ts
@@ -9,13 +9,42 @@ import * as CanvasJS from './canvasjs.min';
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
-  styleUrls: ['./statistics.component.scss']
+  styleUrls: ['./statistics.component.scss'],
 })
 export class StatisticsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-    console.log('CanvasJS: ', CanvasJS);
-  }
+constructor() { }
+signData = []
+onChange(deviceValue) {
+  this.signData = []
+    console.log(deviceValue);
+    if(deviceValue == 1){
+      this.signData = [
+      { y: 1, label: "Corpus" },
+      { y: 5, label: "Portland" },
+      { y: 3, label: "Mathis" },
+      { y: 0, label: "Beeville" },
+      { y: 2, label: "Rockport" }]
+    }
+    else if (deviceValue == 2){
+      this.signData = [
+        { y: 0, label: "Corpus" },
+        { y: 3, label: "Portland" },
+        { y: 2, label: "Mathis" },
+        { y: 0, label: "Beeville" },
+        { y: 1, label: "Rockport" }]
+      }
+    new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: "Potential Clients"
+      },
+      data: [{
+        type: "column",
+        dataPoints: this.signData
+      }]
+    }).render();
+}
+  
+ngOnInit(){}
 }
