691a96b87a8d4d5f6b3941993a4b95ffc2310ef9
Rosa Isela Borrego-Butler <252-rborregobutler@users.noreply.gitlab.tamucc.edu>
2020-04-27 19:11:05 -0500

    Images rendering on homepage and navigation working.

--- client/ui/src/app/pages/home/home.module.ts
+++ client/ui/src/app/pages/home/home.module.ts
@@ -1,27 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-<<<<<<< HEAD
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from "@angular/material/button";
 
 
-=======
 import {StatisticsComponent} from '../statistics/statistics.component';
 import {DisplayComponent} from '../display/display.component';
 import {RouterModule} from '@angular/router'
 import { AngularMaterialModule } from 'src/app/angular-material/angular-material.module';
 import { OnBoardingComponent } from '../on-boarding/on-boarding.component';
->>>>>>> 5c5e0d473447d80516844b8f0709ea212af92955
 
 @NgModule({
   declarations: [HomeComponent], // delare HomeComponent
   imports: [
     CommonModule,
-<<<<<<< HEAD
     MatButtonModule,
-    MatCardModule
-=======
+    MatCardModule,
     AngularMaterialModule,
     RouterModule.forRoot([
       {
@@ -42,7 +37,6 @@ import { OnBoardingComponent } from '../on-boarding/on-boarding.component';
       }
 
     ])
->>>>>>> 5c5e0d473447d80516844b8f0709ea212af92955
   ]
 })
 export class HomeModule { }
