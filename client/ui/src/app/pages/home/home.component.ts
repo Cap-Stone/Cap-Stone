e25260d475d41de23f9ef166a9aa1325decc181c
Ruben <rortiz18@islander.tamucc.edu>
2020-05-04 03:53:09 -0500

    added a user friendly home page

--- client/ui/src/app/pages/home/home.component.ts
+++ client/ui/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 import { Location } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +13,22 @@ export class HomeComponent implements OnInit {
   constructor(
     private router: Router,
     private _location: Location,
+    public sanitizer: DomSanitizer
   ) { }
 
+  url: string = "https://benchmarkfamilyservices.org/";
+  urlSafe: SafeResourceUrl;
+  mobileDevice = false;
+
   ngOnInit() {
+    this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+
+    var ua = navigator.userAgent;
+
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(ua)) {
+      this.mobileDevice = true;
+    }
+       
   }
 
   navigate() {
