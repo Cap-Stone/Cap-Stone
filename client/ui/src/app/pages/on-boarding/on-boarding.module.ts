15d5f7cb46b86b837275e785b470b2d2fe48e5ae
Ruben <rortiz18@islander.tamucc.edu>
2020-05-04 18:10:49 -0500

    added side navigation bar to every page

--- client/ui/src/app/pages/on-boarding/on-boarding.module.ts
+++ client/ui/src/app/pages/on-boarding/on-boarding.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 import { OnBoardingComponent } from './on-boarding.component';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import {RouterModule} from '@angular/router'
 // Angular Material
 import { AngularMaterialModule } from 'src/app/angular-material/angular-material.module';
+import { LoginComponent } from '../login/login.component';
 
 
 @NgModule({
@@ -13,7 +15,14 @@ import { AngularMaterialModule } from 'src/app/angular-material/angular-material
     CommonModule,
     AngularMaterialModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule, 
+    RouterModule.forRoot([
+      {
+        path:'login',
+        component: LoginComponent
+      }
+
+    ])
   ]
 })
 export class OnBoardingModule { }
